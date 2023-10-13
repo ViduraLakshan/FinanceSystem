56c177faf2e9c9967df48cb2fffcb1695bb7c2b0
ViduraLakshan <viduralakshan7@gmail.com>
2023-10-13 22:39:47 +0530

    bug fixed in interest calculation

--- src/main/java/com/financeSystem/account/savings/SavingsAccountDao.java
+++ src/main/java/com/financeSystem/account/savings/SavingsAccountDao.java
@@ -160,6 +160,67 @@ public class SavingsAccountDao {
 		
         return error;
     }
+	
+	public void savingsInterestCalculate() {
+    	
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String getDateNow=java.time.LocalDate.now().toString();
+		String getDate=getDateNow.substring(8);
+		
+		try (Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/fsdb?useSSL=false", "root", "3636");
+
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("select * from savings_accounts where created_date like?")) {
+			preparedStatement.setString(1, "%"+getDate);
+			
+			
+			
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			
+			
+			 while (rs.next()) {
+				 int id=rs.getInt("id");
+				 float balance=rs.getFloat("balance");
+				 float interestRate=rs.getFloat("interestRate");
+				 float interest_earned=rs.getFloat("interest_earned");
+				 float interest_earned_new=interest_earned+((balance*interestRate)/100);
+				 float newBalance=balance+(balance*interestRate)/100;
+				 
+				 
+				 String UPDATE_ACCOUNT_SQL = "UPDATE savings_accounts SET balance = ?, interest_earned = ? WHERE id = ?;";
+				 
+
+				            // Step 2:Create a statement using connection object
+				            PreparedStatement preparedStatement2 = connection.prepareStatement(UPDATE_ACCOUNT_SQL); 
+				            preparedStatement2.setFloat(1, newBalance);
+				            preparedStatement2.setFloat(2, interest_earned_new);
+				            preparedStatement2.setInt(3, id);
+				            
+				            //User userFromDb= new User();
+				            
+				          
+
+				            System.out.println(preparedStatement2);
+				            // Step 3: Execute the query or update query
+				            preparedStatement2.executeUpdate();
+				 
+			 }
+
+		} catch (SQLException e) {
+			// process sql exception
+			printSQLException(e);
+		}
+    }
+	
 	private void printSQLException(SQLException ex) {
         for (Throwable e: ex) {
             if (e instanceof SQLException) {
